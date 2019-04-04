4e736313319445a2729f523cb1d014ea57b3c44a
louxfaure <louxfaure@gmail.com>
2019-04-04 11:34:10 +0200

    Récupère le répertoire du script

--- supprime_bib.sh
+++ supprime_bib.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 my_date=`date +%Y%m%d`
+execPath=$(readlink -f $(dirname $0))
+cd $execPath
 # my_script_rep="/home/scoopadmin/Workspace/Notices_sans_exemplaires/"
-my_log_file=/var/tmp/Alma_script/supprime_bib_$my_date.log
-python3 ./supprime_bib.py > $my_log_file
+my_log_file=$HOME/.Alma/supprime_bib_$my_date.log
+source ../../virtual-env/bin/activate
+python ./supprime_bib.py > $my_log_file
 #perl /home/scoopadmin/Workspace/Transf_quality/test.pl 2> $my_log_file
 if [ $? -ne 0 ]
 	then echo -e "Bonjour,\n Le programme de suppression des notices sans exemplaires  s'est arrété sur un échec. Pour un savoir plus voir les logs sous $my_log_file" | mail -s "Supprime Notices sans exemplaires -- EREUR" alexandre.faure@u-bordeaux.fr
