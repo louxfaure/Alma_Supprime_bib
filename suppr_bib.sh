7c291535ca79e1ef499b611039f12257891c0d60
louxfaure <louxfaure@gmail.com>
2019-04-09 16:27:24 +0200

    Coorction du script bash

--- suppr_bib.sh
+++ suppr_bib.sh
@@ -5,7 +5,7 @@ cd $execPath
 # my_script_rep="/home/scoopadmin/Workspace/Notices_sans_exemplaires/"
 my_log_file=$HOME/.Alma/Logs/supprime_bib_$my_date.log
 source ../../virtual-env/bin/activate
-python3 ./supprime_bib.py > $my_log_file
+python3 ./supprime_bib.py 2> $my_log_file
 #perl /home/scoopadmin/Workspace/Transf_quality/test.pl 2> $my_log_file
 if [ $? -ne 0 ]
 	then echo -e "Bonjour,\n Le programme de suppression des notices sans exemplaires  s'est arrété sur un échec. Pour un savoir plus voir les logs sous $my_log_file" | mail -s "Supprime Notices sans exemplaires -- EREUR" alexandre.faure@u-bordeaux.fr
